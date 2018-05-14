e94cd41768d8c116e4a5579232b2672df29e70af
liuzundong <442111144@qq.com>
2018-05-14 15:36:26 +0800

    ssss

--- SkyNetClent/Assets/Lua/Login/LoginMsgHandler.lua
+++ SkyNetClent/Assets/Lua/Login/LoginMsgHandler.lua
@@ -4,20 +4,25 @@ LoginMsgHandler.MsgId = {
     [2] = "S2CLoginSuccess"
 }
 
+local base_pb = require("Protol.Base_pb")
+
 function LoginMsgHandler:OnMessage(smallId, buffer)
     self[LoginMsgHandler.MsgId[smallId]](buffer)
 end
 
-function LoginMsgHandler:S2CLogin(buffer)   
-    local base_pb=require("Assets.Lua.Proto.Base_pb")
-    local msg= base_pb.C2SLogin()
-    msg.account="mm"
-    msg.pwd="pwd"
-    msg.servernumber=33
-    msg.mac="mac"
-    local data= msg:SerializeToString()
-    TestStart.networkManager:SendMessage(1,1,data)
+function LoginMsgHandler:S2CLogin(buffer)  
+    local msg = base_pb.C2SLogin()
+    msg.account = "mm"
+    msg.pwd = "pwd"
+    msg.servernumber = 33
+    msg.mac = "mac"
+    local data = msg:SerializeToString()
+    TestStart.networkManager:SendMessage(1, 1, data)
 end
 
 function LoginMsgHandler:S2CLoginSuccess(buffer)
+    print("LoginMsgHandler:S2CLoginSuccess")
+    local msg=Protol.Base_pb.S2CLoginSuccess()
+    msg:ParseFromString(buffer)   
+    print("id:",msg.id,"name:",msg.name,"servernumber:",msg.servernumber)
 end
