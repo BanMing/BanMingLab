8cd6c651e5b12ad9cf55d5716e341683be4eee98
BanMing <banming729@163.com>
2020-04-11 16:18:56 +0800

    调整结构

---
+++ CSharpLab1/EventTest/EventTest.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class EventTest {
+    public event Action<string> EventPrintStr;
+
+    public Action<string> ActionPrintStr;
+
+    public void DoEvent () {
+        EventPrintStr.Invoke ("mmmm");
+    }
+
+    public static RunTest(){
+         EventTest eventTest = new EventTest ();
+            eventTest.EventPrintStr += (str) => { Console.WriteLine ("Event Print Str" + str); };
+            // eventTest.PrintStr();
+            eventTest.ActionPrintStr += (str) => { Console.WriteLine ("Action Print Str" + str); };
+            eventTest.ActionPrintStr.Invoke ("sssss");
+            eventTest.DoEvent ();
+    }
+}
