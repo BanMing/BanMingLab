6f81b09afa4f2ea5d0234e05ce4a6a821ccc6f41
BanMing <ban-ming@foxmail.com>
2021-06-10 15:46:10 +0800

    add time test

--- CSharpLab/Program.cs
+++ CSharpLab/Program.cs
@@ -7,7 +7,15 @@ namespace CSharpLab
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            ReferenceTest.Run1();
+            // ReferenceTest.Run1();
+            // int a = 1;
+            // string aa = string.Format("{0}", a); //$"{a}"; 
+            // string b = string.Format("{0}", a);
+            // System.Console.WriteLine(aa.Equals(b));
+            // System.Console.WriteLine(aa == b);
+            System.Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
         }
+
+
     }
 }
