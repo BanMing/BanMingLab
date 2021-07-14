03994dc756ca6ff8f6fdc56b31b9cf080d228dd7
BanMing <ban-ming@foxmail.com>
2021-07-14 16:05:40 +0800

    add string format

--- CSharpLab/Program.cs
+++ CSharpLab/Program.cs
@@ -6,14 +6,20 @@ namespace CSharpLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
             // ReferenceTest.Run1();
             // int a = 1;
             // string aa = string.Format("{0}", a); //$"{a}"; 
             // string b = string.Format("{0}", a);
             // System.Console.WriteLine(aa.Equals(b));
             // System.Console.WriteLine(aa == b);
-            System.Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+            // System.Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+            // System.Random random = new Random();
+            // for (int i = 0; i < 8; i++)
+            // {
+            //     System.Console.WriteLine(random.Next(-10, 2));
+            // }
+            FormatTest.Run();
         }
 
 
---
+++ CSharpLab/String/Format.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class FormatTest
+{
+    public static void Run()
+    {
+        string test = "Hello";
+        Console.WriteLine($"->{"test",10}<-");
+        Console.WriteLine($"->{"test",-10}<-");
+    }
+}
